639b6b6a343530db19e0459fde61036c76c3a359
Juan Sánchez <JuanchoSL@hotmail.com>
2024-06-26 20:35:35 +0200

    simplify tests

--- tests/ConnectionTrait.php
+++ tests/ConnectionTrait.php
@@ -35,50 +35,50 @@ trait ConnectionTrait
             return self::$connections[$connection_type->value];
         }
 
-        if (!static::$git_mode) {
-            $logger = new Logger((new FileRepository(getenv('LOG_FILEPATH')))->setComposer((new PlainText)->setTimeFormat(getenv('LOG_TIMEFORMAT'))));
-            $logger->log('debug', "Creating {type}", ['function' => __FUNCTION__, 'memory' => memory_get_usage(), 'type' => $connection_type->value]);
-        }
         switch ($connection_type) {
             case Engines::TYPE_MYSQLI:
                 $credentials = new DbCredentials(getenv('MYSQL_HOST'), getenv('MYSQL_USERNAME'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
-                $resource = new Mysqli($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Mysqli($credentials);
                 break;
 
             case Engines::TYPE_SQLITE:
                 $path = dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'var';
                 $credentials = new DbCredentials($path, '', '', 'test.db');
-                $resource = new Sqlite($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Sqlite($credentials);
                 break;
 
             case Engines::TYPE_POSTGRE:
                 $credentials = new DbCredentials(getenv('POSTGRES_HOST'), getenv('POSTGRES_USERNAME'), getenv('POSTGRES_PASSWORD'), getenv('POSTGRES_DATABASE'));
-                $resource = new Postgres($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Postgres($credentials);
                 break;
 
             case Engines::TYPE_SQLSRV:
                 $credentials = new DbCredentials(getenv('SQLSRV_HOST'), getenv('SQLSRV_USERNAME'), getenv('SQLSRV_PASSWORD'), getenv('SQLSRV_DATABASE'));
-                $resource = new Sqlserver($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Sqlserver($credentials);
                 break;
 
             case Engines::TYPE_ORACLE:
                 $credentials = new DbCredentials(getenv('ORACLE_HOST'), getenv('ORACLE_USERNAME'), getenv('ORACLE_PASSWORD'), getenv('ORACLE_DATABASE'));
-                $resource = new Oracle($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Oracle($credentials);
                 break;
 
             case Engines::TYPE_ODBC:
                 $credentials = new DbCredentials(getenv('SQLSRV_HOST'), getenv('SQLSRV_USERNAME'), getenv('SQLSRV_PASSWORD'), getenv('SQLSRV_DATABASE'));
-                $resource = new Odbc($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Odbc($credentials);
                 break;
 
             case Engines::TYPE_DB2:
                 $credentials = new DbCredentials(getenv('DB2_HOST'), getenv('DB2_USERNAME'), getenv('DB2_PASSWORD'), getenv('DB2_DATABASE'));
-                $resource = new Db2($credentials, RDBMS::RESPONSE_OBJECT);
+                $resource = new Db2($credentials);
                 break;
         }
-        $resource->setLogger($logger, true);
+        
+        if (!static::$git_mode) {
+            $logger = new Logger((new FileRepository(getenv('LOG_FILEPATH')))->setComposer((new PlainText)->setTimeFormat(getenv('LOG_TIMEFORMAT'))));
+            $logger->log('debug', "Creating {type}", ['function' => __FUNCTION__, 'memory' => memory_get_usage(), 'type' => $connection_type->value]);
+            $resource->setLogger($logger, true);
+        }
         return self::$connections[$connection_type->value] = $resource;
-        return $resource;
     }
 
 
