7472ea667ee10d373e4a82125a02662ecb176cee
Juan Sánchez <JuanchoSL@hotmail.com>
2024-06-29 04:04:14 +0200

    join

--- src/Datamodel/Relations/BelongsToMany.php
+++ src/Datamodel/Relations/BelongsToMany.php
@@ -11,7 +11,7 @@ class BelongsToMany extends AbstractMultipleRetriever
     public function __construct(DataModelInterface $model, string $foreing_field, string $pivot_foreing_field, DataModelInterface $pivot, string $pivot_owner_field, string $owner_value)
     {
         $pivot_table = $pivot->getTableName();
-        $this->relation = $model::where([$pivot_owner_field, $owner_value])->join(["JOIN " . $pivot_table . " ON " . $pivot_table . "." . $pivot_foreing_field . "=" . $model->getTableName() . "." . $foreing_field]);
+        $this->relation = $model::where([$pivot_table.".".$pivot_owner_field, $owner_value])->join(["JOIN " . $pivot_table . " ON " . $pivot_table . "." . $pivot_foreing_field . "=" . $model->getTableName() . "." . $foreing_field]);
     }
 
 }
