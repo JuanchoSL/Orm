07f6f98f689a0a023f231b70575732c0184c7f9a
Juan Sánchez <JuanchoSL@hotmail.com>
2024-09-23 23:48:11 +0200

    unify ttl

--- src/Datamodel/CachedModel.php
+++ src/Datamodel/CachedModel.php
@@ -10,7 +10,7 @@ abstract class CachedModel extends Model
 {
 
     protected $lazyLoad = true;
-
+    protected $ttl = 120;
     static $cache = [];
 
     public static function setCache(CacheInterface $cache, string $conection_name = 'default')
@@ -26,7 +26,7 @@ abstract class CachedModel extends Model
     {
         $result = parent::save();
         if ($result && array_key_exists($this->connection_name, self::$cache)) {
-            self::$cache[$this->connection_name]->set($this->createCacheKey(), json_decode(json_encode($this->values), true), 100);
+            self::$cache[$this->connection_name]->set($this->createCacheKey(), json_decode(json_encode($this->values), true), $this->ttl);
         }
         return $result;
     }
@@ -44,13 +44,13 @@ abstract class CachedModel extends Model
         if (array_key_exists($this->connection_name, self::$cache)) {
             $element = self::$cache[$this->connection_name]->get($this->createCacheKey());
         }
-        if (empty ($element)) {
+        if (empty($element)) {
             $element = parent::load($id);
         } else {
             $this->fill((array) $element);
         }
         if (array_key_exists($this->connection_name, self::$cache)) {
-            self::$cache[$this->connection_name]->set($this->createCacheKey(), $element, 100);
+            self::$cache[$this->connection_name]->set($this->createCacheKey(), $element, $this->ttl);
         }
         return $element;
     }
